97f346098c021c427442b3fe36f10381363d69b9
François GRUCHALA <@FGruchala>
2017-10-26 19:28:38 +0200

    FEAT(Tweet) Service de récupération

--- src/app/shared/service/tweets.service.ts
+++ src/app/shared/service/tweets.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Tweet } from './../model/tweet';
 
 @Injectable()
 export class TweetsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getTweets(): Observable<Array<Tweet>> {
+    return this.httpClient.get<Array<Tweet>>('http://localhost:3002/api/tweet');
+  }
 
 }
