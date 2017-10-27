ec00c3d32e9b0d853a4ccb58e5e4fe4b5664d853
François GRUCHALA <@FGruchala>
2017-10-28 01:12:56 +0200

    FIX(Tweet) Suppression du compteur de reply

--- src/app/shared/model/tweet.ts
+++ src/app/shared/model/tweet.ts
@@ -5,7 +5,6 @@ export class Tweet {
     public text: string;
     public user: User;
     public createdAt: Date;
-    public replyCount: number;
     public retweetCount: number;
     public favoriteCount: number;
 
@@ -13,13 +12,11 @@ export class Tweet {
         text: string,
         user: User,
         createdAt: Date,
-        replyCount: number,
         retweetCount: number,
         favoriteCount: number) {
             this.text = text;
             this.user = user;
             this.createdAt = createdAt;
-            this.replyCount = replyCount;
             this.retweetCount = retweetCount;
             this.favoriteCount = favoriteCount;
     }
