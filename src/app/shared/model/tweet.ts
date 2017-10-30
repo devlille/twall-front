7ba70d349b5b12e24b92dcb2f695bb8a292c0e43
François GRUCHALA <@FGruchala>
2017-10-30 23:20:01 +0100

    FIX(Tweet) Minor changes

--- src/app/shared/model/tweet.ts
+++ src/app/shared/model/tweet.ts
@@ -2,6 +2,7 @@ import { User } from './user';
 
 export class Tweet {
 
+    public id: number;
     public text: string;
     public user: User;
     public createdAt: Date;
@@ -9,11 +10,13 @@ export class Tweet {
     public favoriteCount: number;
 
     constructor(
+        id: number,
         text: string,
         user: User,
         createdAt: Date,
         retweetCount: number,
         favoriteCount: number) {
+            this.id = id;
             this.text = text;
             this.user = user;
             this.createdAt = createdAt;
