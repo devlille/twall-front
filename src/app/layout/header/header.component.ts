665cca3dab4e8e0b33dc539d423b75ce1521bb59
François GRUCHALA <@FGruchala>
2017-12-21 22:46:46 +0100

    :sparkles: add some new language

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  public isChti: Boolean;
+  private _timer: NodeJS.Timer;
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.toggleLang();
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this._timer);
+  }
+
+  private toggleLang(): void {
+    this.isChti = !this.isChti;
+    this._timer = setTimeout(() => this.toggleLang(), 10000);
+  }
 
 }
