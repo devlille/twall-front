c53846af2a0ca2e375ba48c6dc5086dd9287090e
François GRUCHALA <@FGruchala>
2017-12-21 23:32:24 +0100

    :fire: remove red dot

--- src/app/widget/widget.module.ts
+++ src/app/widget/widget.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material';
 
 import { ClockComponent } from './clock/clock.component';
 import { LiveComponent } from './live/live.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [ClockComponent, LiveComponent],
   declarations: [ClockComponent, LiveComponent]
