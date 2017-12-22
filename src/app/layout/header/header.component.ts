5d3cafc76f939a7ace86e7d26d37e52df550564a
François GRUCHALA <@FGruchala>
2017-12-22 21:25:20 +0100

    :lipstick: update header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('easeIn', [
+      transition(
+        ':enter',
+        [
+          style({opacity: 0}),
+          animate('500ms ease-in', style({opacity: 1}))
+        ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
